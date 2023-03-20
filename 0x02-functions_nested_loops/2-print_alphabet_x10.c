7bff040776874026ca99b28851875d423fc0fcd7
Cuthbert10 <Cuthbertmbundi7@gmail.com>
2023-03-20 09:34:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter;
+	char alpha;
 	int counter;
+
 	for (counter = 1; counter <= 10; counter++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-
-	_putchar('\n');
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
 	}
 }
