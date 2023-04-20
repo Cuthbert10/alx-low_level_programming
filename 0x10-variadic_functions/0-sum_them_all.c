52cf1e514920e93193023df9905c249ae8cc0508
Cuthbert10 <Cuthbertmbundi7@gmail.com>
2023-04-20 04:54:38 -0700

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,27 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * sum_them_all - return sum when given unknown amount of arguments
+ * @n: integers to add
+ * Return: sum
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
-va_list arguments;
-int sum = 0;
-unsigned int i;
+	va_list valist;
+	unsigned int sum = 0, i;
 
-/* Initialize the arguments list */
-va_start(arguments, n);
+	/* validate valist and initialize */
+	if (n == 0)
+		return (0);
+	va_start(valist, n);
 
-/* Add up all the arguments */
-for (i = 0; i < n; i++) 
-{
-sum += va_arg(arguments, int);
-}
+	/* iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 
-/* Clean up the arguments list */
-va_end(arguments);
-
-return (sum);
+	return (sum);
 }
 
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,31 +1,28 @@
-#include <stdarg.h>
-#include <stdio.h>
+#include <stdarg.h> 
+#include <stdio.h> 
 #include "variadic_functions.h"
+
+/**
+ * print_numbers - print varying amount of numbers
+ * @separator: delimiter
+ * @n: amount of arguments in list
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list arguments;
-unsigned int i;
-int current_number;
+	va_list valist;
+	unsigned int i;
 
-/* Initialize the arguments list */
-va_start(arguments, n);
-
-/* Print each number with the specified separator */
-for (i = 0; i < n; i++)
-{
-current_number = va_arg(arguments, int);
-printf("%d", current_number);
-if (i != n - 1 && separator != NULL)
-{
-printf("%s", separator);
+	if (n > 0)
+	{
+		va_start(valist, n);
+		for (i = 1; i <= n; i++)
+		{
+			printf("%d", va_arg(valist, int));
+			if (i != n && separator != NULL)
+				printf("%s", separator);
+		}
+		va_end(valist);
+	}
+	printf("\n");
 }
-}
-
-/* Clean up the arguments list */
-va_end(arguments);
-
-/* Print a new line at the end */
-printf("\n");
-}
-
