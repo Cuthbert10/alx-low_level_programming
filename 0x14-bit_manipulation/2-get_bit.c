1a1c2e45069225449b68f93a60128c113d5c1916
Cuthbert10 <Cuthbertmbundi7@gmail.com>
2023-05-05 09:19:24 -0700

    bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 	unsigned int max_bits;
+
 	max_bits = (sizeof(unsigned long int) * 8);
+
 	if (index > max_bits)
 		return (-1);
 	bit = ((n >> index) & 1);
